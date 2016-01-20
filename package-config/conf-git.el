f9b36da6f6a68c8ff16e14c5dab11b2f0cd3aa42
Matt Briggs <matt@mattbriggs.net>
2016-01-20 12:29:30 -0500

    git stuff

---
+++ package-config/conf-git.el
@@ -0,0 +1,9 @@
+(require 'open-github-from-here)
+(setq open-github-from-here:command (expand-file-name "~/.emacs.d/make-github-url-from-file"))
+
+(require 'evil)
+(push 'git-commit-mode evil-emacs-state-modes)
+
+(global-diff-hl-mode +1)
+
+(provide 'conf-git)
